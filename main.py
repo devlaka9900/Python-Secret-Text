35c380ee0e316bb76cfee001c13401c23acf7aaa
Binura <s92072185@ousl.lk>
2026-05-28 00:11:47 +0530

    completed basic project. all functions working. ready for test

--- main.py
+++ main.py
@@ -4,7 +4,62 @@ from tkinter import messagebox
 import base64
 import os
 
+def decrypt():
+    password = code.get()
+
+    if password == "1234":
+        screen2 = Toplevel(screen)
+        screen2.title("decryption")
+        screen2.geometry("400x200")
+        screen2.configure(bg="#00bd56")
+    
+        message=text1.get(1.0, END)
+        decode_message=message.encode("ascii")
+        base64_bytes=base64.b64decode(decode_message)
+        decrypt = base64_message=base64_bytes.decode("ascii")
+
+        Label(screen2,text="DECRYPTION", font="arial", fg="white", bg="#00bd56").place(x=10, y=0)
+        text2=Text(screen2, font="Robote 10", bg="white", relief=GROOVE, wrap=WORD, bd=0)
+        text2.place(x=10, y=40, width=380, height=150)
+
+        text2.insert(END, decrypt)
+    
+    elif password=="":
+        messagebox.showerror("Error", "Please enter a password")
+    else:
+        messagebox.showerror("Error", "Wrong password")
+
+def encrypt():
+    password = code.get()
+
+    if password == "1234":
+        screen1 = Toplevel(screen)
+        screen1.title("encryption")
+        screen1.geometry("400x200")
+        screen1.configure(bg="#ed3833")
+    
+        message=text1.get(1.0, END)
+        encode_message=message.encode("ascii")
+        base64_bytes=base64.b64encode(encode_message)
+        encrypt = base64_message=base64_bytes.decode("ascii")
+
+        Label(screen1,text="ENCRYPT", font="arial", fg="white", bg="#ed3833").place(x=10, y=0)
+        text2=Text(screen1, font="Robote 10", bg="white", relief=GROOVE, wrap=WORD, bd=0)
+        text2.place(x=10, y=40, width=380, height=150)
+
+        text2.insert(END, encrypt)
+    
+    elif password=="":
+        messagebox.showerror("Error", "Please enter a password")
+    else:
+        messagebox.showerror("Error", "Wrong password")
+
 def main_screen():
+    
+    global screen
+    global code
+    global text1
+
     screen = Tk()
     screen.geometry("375x398")
 
@@ -16,12 +71,6 @@ def main_screen():
     def reset():
         code.set("")
         text1.delete(1.0, END)
-
-    def encrypt():
-        pass
-
-    def decrypt():
-        pass
 
     Label(text="Enter text for encription and decryption", fg="black", font=("arial", 13)).place(x=10, y=10)
     text1=Text(font="Robote 20", bg="white", relief=GROOVE, wrap=WORD, bd=0)
